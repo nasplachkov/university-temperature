60088961d7e447e7e266b3fe94c77755a34b9f25
atanasplachkov <atanasplachkov@deltasource.eu>
2013-12-29 16:00:45 +0200

    Posting to twitter with different accounts is working.

--- src/main/java/rest/TwitterServlet.java
+++ src/main/java/rest/TwitterServlet.java
@@ -5,6 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.RequestToken;
 
 /**
  * Servlet implementation class TwitterServlet
@@ -24,14 +30,84 @@ public class TwitterServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String path = request.getRequestURI();
+		HttpSession session = request.getSession();
+		
+		if (path.endsWith("/twitter")) {
+			
+			Object terms = session.getAttribute("terms_accepted");
+			if (terms == null) {
+				terms = false;
+			}
+			
+			if ((Boolean) terms) {
+				// show app page
+				request.getRequestDispatcher("/WEB-INF/public_hidden/twitter.html").forward(request, response);
+			} else {
+				// show terms page
+				request.getRequestDispatcher("/WEB-INF/public_hidden/terms.html").forward(request, response);
+			}
+			
+		} else {
+			// Twitter callback
+			Twitter twitter = (Twitter) session.getAttribute("twitter");
+			RequestToken requestToken = (RequestToken) session.getAttribute("requestToken");
+			try {
+				twitter.getOAuthAccessToken(requestToken, request.getParameter("oauth_verifier"));
+				
+				session.removeAttribute("requestToken");
+				
+				response.sendRedirect(request.getContextPath() + "/twitter");
+			} catch (TwitterException e) {
+				throw new ServletException(e);
+			}
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String path = request.getRequestURI();
+		HttpSession session = request.getSession();
+		
+		if (path.endsWith("/twitter")) {
+			
+			if (request.getParameter("update") == null) {
+				String accepted = request.getParameter("terms_accept");
+				
+				if (accepted.equals("true")) {
+					session.setAttribute("terms_accepted", true);
+					
+					Twitter twitter = TwitterFactory.getSingleton();
+					try {
+						String baseUrl = String.format("%s://%s:%d%s", request.getScheme(), request.getServerName(), request.getServerPort(), request.getContextPath());
+						RequestToken rt = twitter.getOAuthRequestToken(baseUrl + "/twitter/callback");
+						
+						session.setAttribute("twitter", twitter);
+						session.setAttribute("requestToken", rt);
+						
+						response.sendRedirect(rt.getAuthenticationURL());
+					} catch (TwitterException e) {
+						e.printStackTrace();
+					}
+				} else {
+					response.sendError(400);
+				}
+			} else {
+				String status = request.getParameter("status");
+				
+				if (status.length() > 0) {
+					Twitter twitter = (Twitter) session.getAttribute("twitter");
+					
+					try {
+						twitter.updateStatus(status);
+					} catch (TwitterException e) {
+						throw new ServletException(e);
+					}
+				}
+			}
+		}
 	}
 
 }
---
+++ src/main/webapp/js/poster.js
@@ -0,0 +1,55 @@
+(function ($) {
+	"use strict";
+	
+	var startButton = $("#start"),
+		log = $("#log"),
+		intervalField = $("#post-interval"),
+		running = false,
+		timerId = null,
+		updateStatus,
+		createStatus;
+	
+	updateStatus = function (status) {
+		$.ajax("/temperature/twitter?update=true", {
+			type: "POST",
+			data: {
+				status: status
+			}
+		}).done(function () {
+			log.append("Posting success!<br/>");
+		}).fail(function () {
+			log.append("Posting failure.<br/>");
+		});
+	};
+	
+	createStatus = function () {
+		$.getJSON("/temperature/rest/get", function (data) {
+			var twitterStatus = "";
+			
+			for (var i in data.list) {
+				var temp = data.list[i];
+				
+				twitterStatus += temp.placeName + ": " + temp.temperature + " °C, " + temp.humidity + " %.\n";
+			}
+			
+			updateStatus(twitterStatus);
+		});
+	};
+	
+	startButton.click(function () {
+		if (running) {
+			clearInterval(timerId);
+			log.append("Timer stopped.<br/>");
+		} else {
+			log.append("Timer started.<br/>");
+			
+			// Update immediately
+			createStatus();
+			
+			// Get the input value and convert it from minutes to milliseconds
+			timerId = setInterval(createStatus, intervalField.val() * 60 * 1000);
+		}
+		running = !running;
+	});
+	
+}(jQuery));
